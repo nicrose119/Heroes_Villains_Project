57202251e1a5ec7c28584173505247690cb400b6
Nicole Overholt <nicrose119@gmail.com>
2023-01-12 04:30:40 -0500

    Created function to update a specific Super

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -16,12 +17,14 @@ def supers_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def super_detail(request, pk):
-    try:
-        super = Supers.objects.get(pk=pk)
+    super = get_object_or_404(Supers, pk=pk)
+    if request.method == 'GET':
         serializer = SupersSerializer(super)
         return Response(serializer.data)
-    except Supers.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    return Response(pk)
+    elif request.method == 'PUT':
+        serializer = SupersSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
