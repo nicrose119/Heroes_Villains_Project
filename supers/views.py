1695ed010d6f2c750a93042865ee561d355a4d7a
Nicole Overholt <nicrose119@gmail.com>
2023-01-12 04:04:20 -0500

    Modified settings in project file and updated views in Supers app

--- supers/views.py
+++ supers/views.py
@@ -1,12 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import SupersSerializer
 from .models import Supers
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
-    supers = Supers.objects.all()
+    if request.method == 'GET':
+        supers = Supers.objects.all()
+        serializer = SupersSerializer(supers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    serializer = SupersSerializer(supers, many=True)
-    
-    return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = SupersSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
